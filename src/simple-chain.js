b93a4190a1cd5240d429dd804d188cecefe5ab05
Ekaterina <katepthv12@gmail.com>
2023-12-17 19:49:05 +0100

    feat: solve simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -8,27 +8,46 @@ const chainMaker = {
   arr: [],
 
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
 
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    value = String(value);
+
+    if (value) {
+      this.arr.push(`( ${value} )`);
+    } else {
+      this.arr.push(`( )`);
+    }
+
+    return this;
   },
 
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      position <= 0 ||
+      position > this.arr.length ||
+      !Number.isInteger(position) ||
+      position === undefined
+    ) {
+      this.arr = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+      
+    this.arr.splice(position - 1, 1);
+    
+    return this;
+
   },
 
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const x = this.arr.join('~~');
+    this.arr = [];
+    return x;
   }
 };
 
