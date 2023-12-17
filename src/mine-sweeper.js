eab6f28f9337e3928ac839478c47642e2f6e2f46
Ekaterina <katepthv12@gmail.com>
2023-12-17 18:19:19 +0100

    feat: solve mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,179 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = [];
+
+    matrix.forEach((arr, index) => {
+      if (index === 0) {
+        let subsres = Array(3).fill(0);
+        arr.forEach((item, i) => {
+          if (i === 0) {
+            if (arr[i + 1]) {
+              subsres[0] += 1;
+            }
+            if (matrix[1][i]) {
+              subsres[0] += 1;
+            }
+            if (matrix[1][i + 1]) {
+              subsres[0] += 1;
+            }
+          }
+
+          if (i === 1) {
+            if (arr[i - 1]) {
+              subsres[1] += 1;
+            }
+            if (arr[i + 1]) {
+              subsres[1] += 1;
+            }
+            if (matrix[1][i]) {
+              subsres[1] += 1;
+            }
+            if (matrix[1][i - 1]) {
+              subsres[1] += 1;
+            }
+            if (matrix[i][i + 1]) {
+              subsres[1] += 1;
+            }
+          }
+
+          if (i === 2) {
+            if (arr[i - 1]) {
+              subsres[2] += 1;
+            }
+            if (matrix[1][i]) {
+              subsres[2] += 1;
+            }
+            if (matrix[1][i - 1]) {
+              subsres[2] += 1;
+            }
+          }
+        });
+        result.push(subsres);
+      } else if (index === 1) {
+        let subsres = Array(3).fill(0);
+        arr.forEach((item, i) => {
+          if (i === 0) {
+            if (arr[i + 1]) {
+              subsres[0] += 1;
+            }
+            if (matrix[0][i]) {
+              subsres[0] += 1;
+            }
+            if (matrix[0][i + 1]) {
+              subsres[0] += 1;
+            }
+            if (matrix[2]) {
+              if (matrix[2][i + 1]) {
+                subsres[0] += 1;
+              }
+              if (matrix[2][i]) {
+                subsres[0] += 1;
+              }
+            }
+          }
+
+          if (i === 1) {
+            if (arr[i - 1]) {
+              subsres[1] += 1;
+            }
+            if (arr[i + 1]) {
+              subsres[1] += 1;
+            }
+            if (matrix[0][i]) {
+              subsres[1] += 1;
+            }
+            if (matrix[0][i - 1]) {
+              subsres[1] += 1;
+            }
+            if (matrix[0][i + 1]) {
+              subsres[1] += 1;
+            }
+            if (matrix[2]) {
+              if (matrix[2][i]) {
+                subsres[1] += 1;
+              }
+              if (matrix[2][i - 1]) {
+                subsres[1] += 1;
+              }
+              if (matrix[2][i + 1]) {
+                subsres[1] += 1;
+              }
+            }
+          }
+
+          if (i === 2) {
+            if (arr[i - 1]) {
+              subsres[2] += 1;
+            }
+            if (matrix[0][i + 1]) {
+              subsres[2] += 1;
+            }
+            if (matrix[2]) {
+              if (matrix[2][i]) {
+                subsres[2] += 1;
+              }
+              if (matrix[2][i - 1]) {
+                subsres[2] += 1;
+              }
+            }
+          }
+        });
+        result.push(subsres);
+      } else if (index === 2) {
+        let subsres = Array(3).fill(0);
+        arr.forEach((item, i) => {
+          if (i === 0) {
+            if (arr[i + 1]) {
+              subsres[0] += 1;
+            }
+            if (matrix[1][i]) {
+              subsres[0] += 1;
+            }
+            if (matrix[1][i + 1]) {
+              subsres[0] += 1;
+            }
+          }
+
+          if (i === 1) {
+            if (arr[i - 1]) {
+              subsres[1] += 1;
+            }
+            if (arr[i + 1]) {
+              subsres[1] += 1;
+            }
+            if (matrix[1][i]) {
+              subsres[1] += 1;
+            }
+            if (matrix[1][i - 1]) {
+              subsres[1] += 1;
+            }
+            if (matrix[1][i + 1]) {
+              subsres[1] += 1;
+            }
+          }
+
+          if (i === 2) {
+            if (arr[i - 1]) {
+              subsres[2] += 1;
+            }
+            if (matrix[1][i]) {
+              subsres[2] += 1;
+            }
+            if (matrix[1][i - 1]) {
+              subsres[2] += 1;
+            }
+          }
+        });
+
+        result.push(subsres);
+      }
+    });
+  
+  return result;
 }
+
 
 module.exports = {
   minesweeper
